5ed05b6dfdaa5d04aca02f10e322be664569746d
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-08-08 18:11:40 +0900

    add day card component

---
+++ wws-client/src/components/calendar/DayCard.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let day;
+  export let today;
+</script>
+
+{#if day}
+  <card class={today == day ? "dayCard today" : "dayCard"}>
+    <h4>{day}</h4>
+  </card>
+{:else}
+  <card class="emptyCard" />
+{/if}
+
+<style lang="scss">
+  card {
+    width: 6em;
+    height: 6em;
+    padding: 2em;
+    border-radius: 10px;
+
+    &.dayCard {
+      border: 1px solid gray;
+
+      &.today {
+        background-color: rgb(250, 82, 82);
+      }
+    }
+    &.emptyCard {
+      border: 1px solid rgb(59, 59, 59);
+    }
+  }
+</style>
