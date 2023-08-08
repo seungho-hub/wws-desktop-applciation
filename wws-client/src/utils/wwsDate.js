3399a995d5cb839408886a3656e7ea0e74ac01aa
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-08-08 18:09:41 +0900

    add wwsDate

---
+++ wws-client/src/utils/wwsDate.js
@@ -0,0 +1,14 @@
+class wwsDate extends Date {
+  constructor(...args) {
+    super(...args);
+    this.year = this.getFullYear();
+    this.month = this.getMonth();
+    this.monthName = this.toLocaleString('en-US', { month: 'long' });
+    this.day = this.getDate();
+    this.daysInMonth = new Date(this.year, this.month, 0).getDate();
+
+    this.firstDaysOfWeek = new Date(this.year, this.month - 1, 1).getDay();
+  }
+}
+
+module.exports = wwsDate;
