7390ef68e31c2aefdd6d7a62506e20dde50175ff
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-08-08 18:10:37 +0900

    add calendar page

---
+++ wws-client/src/components/calendar/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Calendar from "./Calendar.svelte";
+  import wwsDate from "../../utils/wwsDate";
+  const currentDate = new wwsDate(2023, 3);
+</script>
+
+<main>
+  <Calendar {currentDate} />
+</main>
+
+<style lang="scss">
+</style>
